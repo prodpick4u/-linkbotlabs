e2d475a926b769aa09f48045084c1bcf24f5216c
prodpick4u <omnistall70@gmail.com>
2025-07-21 16:29:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ from blog_generator import generate_blog_post
 
 if __name__ == "__main__":
     generate_blog_post()
+    from utils.amazon_scraper import get_top_3_products
+    category_url = "https://www.amazon.com/Best-Sellers-Kitchen/zgbs/kitchen"
+top3 = get_top_3_products(category_url)
+
+for product in top3:
+    print(product["title"])
+    print(product["link"])
+    print(product["image"])
+    print("-----")
