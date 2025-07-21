abb08fcb60c05e9adfe460c9e432ccd2ca53bc8c
prodpick4u <omnistall70@gmail.com>
2025-07-21 16:13:22 +0700

    Delete amazon_scraper.py

--- amazon_scraper.py
+++
@@ -1,6 +0,0 @@
-def get_top_products(niche):
-    return [
-        {"title": "Toothbrush A", "price": "$59.99", "url": "https://amzn.to/sample1"},
-        {"title": "Toothbrush B", "price": "$39.99", "url": "https://amzn.to/sample2"},
-        {"title": "Toothbrush C", "price": "$79.99", "url": "https://amzn.to/sample3"},
-    ]
