b5f068be79310b07f626a459488efb566da52a24
prodpick4u <omnistall70@gmail.com>
2025-07-26 11:11:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
             category_title=title,
             markdown=markdown,
             html=html,
-            html_filename=f"docs/posts/post-{slug}.html"  # ✅ Fixed path here
+            html_filename=os.path.join("docs", "posts", f"post-{slug}.html")  # ✅ Fixed path here
         )
 
         print(f"✅ Blog generated for: {title}")
